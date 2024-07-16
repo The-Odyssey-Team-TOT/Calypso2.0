180c8e137a2586913e13e4de11dab3c7a4c55838
Dimitri Almon <dimitri.almon@gmail.com>
2024-07-16 17:23:13 +0200

    Update front

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,5 +3,6 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-import "./globe.js"
+// import "./globe.js"
 import "@rails/actioncable"
+//= require globe
