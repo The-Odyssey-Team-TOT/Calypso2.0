dff16baf5ae4d729dcaf1a2f03003df640dba62f
Dimitri Almon <dimitri.almon@gmail.com>
2024-07-16 15:02:29 +0200

    update home

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,8 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+
+
+pin "three", to: "https://cdn.jsdelivr.net/npm/three@0.149.0/build/three.module.js"
+pin "three/addons/OrbitControls", to: "https://cdn.jsdelivr.net/npm/three@0.149.0/examples/jsm/controls/OrbitControls.js"
+pin "@rails/actioncable", to: "actioncable.esm.js"
