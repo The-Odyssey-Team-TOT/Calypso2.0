581998dd1f143269f2923f54501e39899fc8dcee
Mathieu Popescu <popescu_mathieu@yahoo.com>
2024-07-16 15:40:05 +0200

    production
    Signed-off-by: Mathieu Popescu <popescu_mathieu@yahoo.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,9 @@ Rails.application.configure do
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
+  #
+  config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] }
+
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
