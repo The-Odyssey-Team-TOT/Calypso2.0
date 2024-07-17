6a53474ceac4e803de86a00e4a4be273b1c83f33
Mathieu Popescu <popescu_mathieu@yahoo.com>
2024-07-17 15:13:15 +0200

    added the language_levels validation
    Signed-off-by: Mathieu Popescu <popescu_mathieu@yahoo.com>

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -4,5 +4,8 @@ class Chatroom < ApplicationRecord
   has_many :posts, through: :wall
   has_one_attached :photo
 
+
+  LANGUAGE_LEVEL = ["beginner", "medium", "advanced", "fluent"]
   validates :name, :status, :language, :language_level, presence: true
+  validates :language_level, inclusion: { in: LANGUAGE_LEVEL }
 end
