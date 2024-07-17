d92af8dedaf75cd4c9140c3903476f6100706fbe
Mathieu Popescu <popescu_mathieu@yahoo.com>
2024-07-17 15:07:05 +0200

    added the validations
    Signed-off-by: Mathieu Popescu <popescu_mathieu@yahoo.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :wall
   belongs_to :user
   has_one_attached :photo
+
+  validates :content, presence: true, length: { minimum: 10, maximum: 1000}
 end
--- app/models/wall.rb
+++ app/models/wall.rb
@@ -1,3 +1,4 @@
 class Wall < ApplicationRecord
   belongs_to :chatroom
+  has_many :posts
 end
