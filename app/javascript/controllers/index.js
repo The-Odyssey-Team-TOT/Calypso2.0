43693b2837a78ea14e0d28d6f3730745e5ab19d6
Dimitri Almon <dimitri.almon@gmail.com>
2024-07-19 12:08:02 +0200

    Update render msg

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,23 @@
 // Import and register all your controllers from the importmap under controllers/*
 
-import { application } from "./application"
+import { application } from "./application";
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+eagerLoadControllersFrom("controllers", application);
 
-
-// Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
-
+// Import GlobeController if necessary
 // import GlobeController from "./globe_controller";
 // application.register("globe", GlobeController);
+
+document.addEventListener('turbo:load', () => {
+  const messageForm = document.getElementById('new_message');
+  const messageContent = document.getElementById('message_content');
+
+  if (messageForm && messageContent) {
+    messageContent.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault();
+        messageForm.requestSubmit();  // Use requestSubmit() to trigger the form submission
+      }
+    });
+  }
+});
