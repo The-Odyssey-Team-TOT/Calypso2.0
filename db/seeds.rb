aa25faa591e01442fc4509b7ab9e04b546d14fd6
Mathieu Popescu <popescu_mathieu@yahoo.com>
2024-07-19 12:24:19 +0200

    y
    Signed-off-by: Mathieu Popescu <popescu_mathieu@yahoo.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,46 +9,46 @@
 #   end
 
 
-# chatroom1 = Chatroom.create(name: "Calypso", password: "", status: "private", language:"french", language_level:"advanced", topic: "anime", security_level: "solid")
+chatroom1 = Chatroom.create(name: "Calypso", password: "", status: "private", language:"french", language_level:"advanced", topic: "anime", security_level: "solid")
 
-# User.create!(email: 'user1@example.com',
-#   nickname: 'Alex',
-#   description: 'I am a Software Engineer at Google',
-#   spoken_language: 'Spanish',
-#   learning_language: 'German',
-#   password: 'password1')
+User.create!(email: 'user1@example.com',
+  nickname: 'Alex',
+  description: 'I am a Software Engineer at Google',
+  spoken_language: 'Spanish',
+  learning_language: 'German',
+  password: 'password1')
 
 
-# User.create!(email: 'user2@example.com',
-#   nickname: 'Ryan',
-#   description: 'I am a Graphic Designer at Microsoft',
-#   spoken_language: 'Japanese',
-#   learning_language: 'English',
-#   password: 'password2')
+User.create!(email: 'user2@example.com',
+  nickname: 'Ryan',
+  description: 'I am a Graphic Designer at Microsoft',
+  spoken_language: 'Japanese',
+  learning_language: 'English',
+  password: 'password2')
 
-# User.create(email: 'user2@example.com') do |user|
-#   user.nickname = 'Ryan'
-#   user.description = 'I am a Graphic Designer at Microsoft'
-#   user.spoken_language = 'Japanese'
-#   user.learning_language = 'English'
-#   user.password = 'password2'
-# end
+User.create(email: 'user2@example.com') do |user|
+  user.nickname = 'Ryan'
+  user.description = 'I am a Graphic Designer at Microsoft'
+  user.spoken_language = 'Japanese'
+  user.learning_language = 'English'
+  user.password = 'password2'
+end
 
-# User.create(email: 'user3@example.com') do |user|
-#   user.nickname = 'Michael'
-#   user.description = 'I am a baker in Marseille'
-#   user.spoken_language = 'French'
-#   user.learning_language = 'Polish'
-#   user.password = 'password3'
-# end
+User.create(email: 'user3@example.com') do |user|
+  user.nickname = 'Michael'
+  user.description = 'I am a baker in Marseille'
+  user.spoken_language = 'French'
+  user.learning_language = 'Polish'
+  user.password = 'password3'
+end
 
-# User.create(email: 'user4@example.com') do |user|
-#   user.nickname = 'Fabiola'
-#   user.description = 'I am a big companys CEO'
-#   user.spoken_language = 'Mandarin'
-#   user.learning_language = 'Dutch'
-#   user.password = 'password4'
-# end
+User.create(email: 'user4@example.com') do |user|
+  user.nickname = 'Fabiola'
+  user.description = 'I am a big companys CEO'
+  user.spoken_language = 'Mandarin'
+  user.learning_language = 'Dutch'
+  user.password = 'password4'
+end
 
 
 Wall.create!(chatroom_id: Chatroom.first.id)
