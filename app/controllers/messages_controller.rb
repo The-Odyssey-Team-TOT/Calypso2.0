55037e76afeffb35ba0fc60009f78382681e983c
Mathieu Popescu <popescu_mathieu@yahoo.com>
2024-07-25 18:17:00 +0200

    notif
    Signed-off-by: Mathieu Popescu <popescu_mathieu@yahoo.com>

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,7 +12,7 @@ class MessagesController < ApplicationController
       #   format.js { render 'messages/create' }
       #   format.html { redirect_to chatroom_path(@chatroom) }
       # end
-      @chatroom.users.where.not(id: current_user.id).each do |user|
+      #@chatroom.users.where.not(id: current_user.id).each do |user|
         @notification = Notification.create(
           user: current_user,
           recipient: current_user,
@@ -20,7 +20,7 @@ class MessagesController < ApplicationController
           action: "sent"
         )
       NotificationsChannel.broadcast_to(current_user,  render_to_string(partial: "notifications/notification", locals: {notification: @notification}))
-      end
+      # end
       head :ok
     else
       respond_to do |format|
